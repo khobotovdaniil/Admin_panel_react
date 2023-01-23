2125bdf768f1e4c8dc2aef1a60fdbb0b6a054bc9
Daniil_Khobotov <dinich44@gmail.com>
2023-01-23 13:46:48 +0400

    add react

---
+++ app/src/components/editor/editor.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import React, {Component} from "react";
+
+
+export default class Editor extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageList: [],
+            newPageName: ""
+        }
+        this.createNewPage= this.createNewPage.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadPageList();
+    }
+
+    loadPageList() {
+        axios
+            .get("./api")
+            .then(res => this.setState({pageList: res.data}))
+    }
+
+    createNewPage() {
+        axios
+            .post("./api/createNewPage.php", {"name": this.state.newPageName})
+            .then(this.loadPageList())
+            .catch(() => alert("Страница уже существует!"));
+    }
+
+    render() {
+        const {pageList} = this.state;
+        const pages = pageList.map((page, i) => {
+            return (
+                <h1 key={i}>{page}</h1>
+            )
+        });
+
+        return (
+            <>
+                <input
+                    onChange={e => {this.setState({newPageName: e.target.value})}}
+                    type="text"/>
+                <button onClick={this.createNewPage}>Создать страницу</button>
+                {pages}
+            </>
+        )
+    }
+}
--- app/src/main.js
+++ app/src/main.js
@@ -1,23 +1,29 @@
-import $ from 'jquery';
+import React from "react";
+import ReactDOM  from "react-dom";
+import Editor from './components/editor';
 
-function getPageList() {
-    $("h1").remove();
-    $.get('./api', data => {
-        data.forEach(file => {
-            $('body').append(`<h1>${file}</h1>`)
-        })
-    }, 'JSON')
-}
+ReactDOM.render(<Editor/> , document.getElementById('root'));
 
-getPageList();
 
-$('button').click(() => {
-    $.post("./api/createNewPage.php", {
-        "name": $("input").val()
-    }, () => {
-        getPageList();
-    })
-    .fail(() => {
-        alert("Страница уже существует!");
-    })
-})
+
+// function getPageList() {
+//     $("h1").remove();
+//     $.get('./api', data => {
+//         data.forEach(file => {
+//             $('body').append(`<h1>${file}</h1>`)
+//         })
+//     }, 'JSON')
+// }
+
+// getPageList();
+
+// $('button').click(() => {
+//     $.post("./api/createNewPage.php", {
+//         "name": $("input").val()
+//     }, () => {
+//         getPageList();
+//     })
+//     .fail(() => {
+//         alert("Страница уже существует!");
+//     })
+// })
