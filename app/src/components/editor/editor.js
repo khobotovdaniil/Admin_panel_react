381bfa514b96846caf6653fc20cd9fbb33e1fcbe
Daniil_Khobotov <dinich44@gmail.com>
2023-01-24 14:27:39 +0400

    add UIkit and modal for saving

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,7 +1,12 @@
 import "../../helpers/iframeLoader.js";
+
 import axios from "axios";
 import React, {Component} from "react";
+import UIkit from "uikit";
+
 import DOMHelper from "../../helpers/dom-helper.js";
+import EditorText from "../editor-text"
+import Spinner from "../spinner";
 
 export default class Editor extends Component {
     constructor() {
@@ -10,9 +15,12 @@ export default class Editor extends Component {
         this.currentPage = "index.html";
         this.state = {
             pageList: [],
-            newPageName: ""
+            newPageName: "",
+            loading: true
         }
-        this.createNewPage= this.createNewPage.bind(this);
+        this.createNewPage = this.createNewPage.bind(this);
+        this.isLoading = this.isLoading.bind(this);
+        this.isLoaded = this.isLoaded.bind(this);
     }
 
     componentDidMount() {
@@ -21,11 +29,11 @@ export default class Editor extends Component {
 
     init(page) {
         this.iframe = document.querySelector('iframe');
-        this.open(page);
+        this.open(page, this.isLoaded);
         this.loadPageList();
     }
 
-    open(page) {
+    open(page, callback) {
         this.currentPage = page;
 
         axios
@@ -40,29 +48,45 @@ export default class Editor extends Component {
             .then(html => axios.post("./api/saveTempPage.php", {html}))
             .then(() => this.iframe.load("../temp.html"))
             .then(() => this.enableEditing())
+            .then(() => this.injectStyles())
+            .then(callback)
     }
 
     enableEditing() {
         this.iframe.contentDocument.body.querySelectorAll("text-editor").forEach(element => {
-            element.contentEditable = 'true';
-            element.addEventListener("input", () => {
-                this.onTextEdit(element);
-            })
+            const id = element.getAttribute("nodeid");
+            const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+
+            new EditorText(element, virtualElement);
         });
     }
 
-    save() {
+    injectStyles() {
+        const style = this.iframe.contentDocument.createElement("style");
+        style.innerHTML = `
+            text-editor:hover {
+                outline: 3px solid orange;
+                outline-offset: 8px;
+            }
+            text-editor:focus {
+                outline: 3px solid red;
+                outline-offset: 8px;
+            }
+        `;
+        this.iframe.contentDocument.head.appendChild(style);
+    }
+
+    save(onSuccess, onError) {
+        this.isLoading();
         const newDom = this.virtualDom.cloneNode(this.virtualDom);
         DOMHelper.unwrapTextNodes(newDom);
         const html = DOMHelper.serializeDomToString(newDom);
 
         axios
             .post("./api/savePage.php", {pageName: this.currentPage, html})
-    }
-
-    onTextEdit(element) {
-        const id = element.getAttribute("nodeid");
-        this.virtualDom.body.querySelector(`[nodeid="${id}"]`).innerHTML = element.innerHTML;
+            .then(onSuccess)
+            .catch(onError)
+            .finally(this.isLoaded);
     }
 
     loadPageList() {
@@ -85,30 +109,58 @@ export default class Editor extends Component {
             .catch(() => alert("Страница не существует!"));
     }
 
+    isLoading() {
+        this.setState({
+            loading: true
+        })
+    }
+
+    isLoaded() {
+        this.setState({
+            loading: false
+        })
+    }
+
     render() {
-        // const {pageList} = this.state;
-        // const pages = pageList.map((page, i) => {
-        //     return (
-        //         <h1 key={i}>{page}
-        //             <a
-        //                 href="#"
-        //                 onClick={() => this.deletePage(page)}>(x)</a>
-        //         </h1>
-        //     )
-        // });
+        const {loading} = this.state;
+        const modal = true;
+        let spinner;
+
+        loading ? spinner = <Spinner active/> : spinner = <Spinner />
 
         return (
             <>
-                <button onClick={() => this.save()}>Save</button>
                 <iframe src={this.currentPage} frameBorder="0"></iframe>
+                
+                {spinner}
+
+                <div className="panel">
+                    <button className="uk-button uk-button-primary" uk-toggle="target: #modal-save">Опубликовать</button>
+
+                </div>
+                {/* <button onClick={() => this.save()}>Save</button> */}
+
+                <div id="modal-save" uk-modal={modal.toString()}>
+                    <div className="uk-modal-dialog uk-modal-body">
+                        <h2 className="uk-modal-title">Сохранение</h2>
+                        <p>Вы действительно хотите сохранить изменения?</p>
+                        <p className="uk-text-right">
+                            <button 
+                                className="uk-button uk-button-default uk-modal-close uk-modal-close"
+                                type="button">Отменить</button>
+                            <button
+                                className="uk-button uk-button-primary uk-modal-close"
+                                type="button"
+                                onClick={() => this.save(() => {
+                                    UIkit.notification({message: 'Успешно сохранено', status: 'success'})
+                                },
+                                () => {
+                                    UIkit.notification({message: 'Ошибка сохранения', status: 'danger'})
+                                })}>Опубликовать</button>
+                        </p>
+                    </div>
+                </div>
             </>
-            // <>
-            //     <input
-            //         onChange={e => {this.setState({newPageName: e.target.value})}}
-            //         type="text"/>
-            //     <button onClick={this.createNewPage}>Создать страницу</button>
-            //     {pages}
-            // </>
         )
     }
 }
